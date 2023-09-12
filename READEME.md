626afaa5b59d9e4d41509b5c7b96646d842acead
miottofelipee <123502192+miottofelipee@users.noreply.github.com>
2023-09-12 10:55:48 -0300

    first commit

---
+++ READEME.md
@@ -0,0 +1,27 @@
+## Cadastro de Equipes
+### Passo a passo
+- [x] Estrutura basica de pastas
+- [x] Criar os inputs
+- [x] Testar o link do JS  possiveis erros: caminho do JS errado
+- [x] Testar inputs  possiveis erros: Falta de chamada no onclick, IDs errados
+- [x] Criar classe da equipe com parte dos atibutos
+- [x] Testar a criação de uma nova instancia de equipe
+- [x] Criar o restante dos atributos da classe 
+- [x] Testar a criação de uma nova instancia completa da equipe  
+possiveis erros: Falta de conversão, divergencia de nome da classe, passagem de parametros
+- [x] Criar classe de serviços da equipe
+- [x] Criar a instacia da classe de serviços
+- [x] Metodo para adicionar uma nova equipe
+- [X] Testar o metodo de adicionar uma nova equipe  
+possiveis erros: Falta de chamada do metodo, parametros errados, divergencia de nome da classe,  letras minusculas e maiusculas, passagem de parametros
+- [x] metodo para listar equipes
+- [x] metodo para listar equipes unicas
+- [x] função para listar equipes
+- [x] função para listar equipes unicas
+- [x] testar funçoes
+- [x] criar div no html para lista de equipes
+- [x] alimentaçao da lista de equipes 
+- [x] funçao para listar equipe unica
+- [x] criar metetodo para editar 
+- [x] 
+- [x] 
---
+++ assets/js/index.js
@@ -0,0 +1,144 @@
+function criarEquipe() {
+    let nome = document.getElementById("nomeDaEquipe").value
+    let quantidade = Number(document.getElementById("quantidade").value)
+    const novaEquipe = new Equipe(nome, quantidade)
+    console.log(novaEquipe);
+    equipeService.adicionarEquipe(novaEquipe);
+    listarEquipes();
+}
+
+class Equipe {
+    constructor(nome, quantidade) {
+        this.id = this.gerarId();
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.reserva = this.calcularReservas();
+        this.totaldeJogadores = this.totaldeJogadores();
+    }
+
+    gerarId() {
+        return Math.floor(Math.random() * 1000);
+    }
+
+    calcularReservas() {
+        return Math.floor(this.quantidade / 2);
+    }
+
+    totaldeJogadores() {
+        return this.quantidade + this.reserva;
+    }
+}
+
+class EquipeService {
+    constructor() {
+        this.equipes = [];
+    }
+
+    adicionarEquipe(parametro) {
+        this.equipes.push(parametro)
+    }
+
+    listarEquipes() {
+        return this.equipes;
+    }
+
+    listarEquipesPorId(parametro) {
+        return this.equipes.find(equipe => equipe.id == parametro);
+    }
+
+    atualizarEquipes(id, nome, quantidade) {
+        const equipe = this.listarEquipesPorId(id);
+        equipe.nome = nome;
+        equipe.quantidade = quantidade;
+        equipe.reserva = equipe.calcularReservas();
+        equipe.totaldeJogadores = equipe.totaldeJogadores();
+
+        return equipe;
+    }
+
+    deletarEquipe(parametro){
+        return(this.equipes = this.equipes.filter(
+            (equipe) => equipe.id != parametro
+        ))
+    }
+}
+
+const equipeService = new EquipeService();
+
+function listarEquipes() {
+    const equipes = equipeService.listarEquipes();
+    console.log(equipes);
+
+    const elementoLista = document.getElementById("listarEquipes");
+    elementoLista.innerHTML = '';
+    let content = '';
+    equipes.forEach((equipe) => {
+        content += `
+        <div onclick="listarEquipesPorId(${equipe.id})">
+        <p> Nome: ${equipe.nome}</p>
+        </div>
+        `
+    }
+    )
+    elementoLista.innerHTML = content;
+}
+
+function listarEquipesPorId(id) {
+    const equipe = equipeService.listarEquipesPorId(id);
+    console.log(equipe)
+
+    const elementoLista = document.getElementById("listarEquipesUnica");
+    elementoLista.innerHTML = '';
+
+    let content = `
+        <div>
+        <p> Id: ${equipe.id}</p>
+        <p> Nome: ${equipe.nome}</p>
+        <p> Total de jogadores: ${equipe.totaldeJogadores}</p>
+        <p> Titulares: ${equipe.quantidade}</p>
+        <p> Reservas: ${equipe.reserva}</p>
+        <button onclick="atualizarEquipes(${equipe.id})">Editar</button>
+        <button onclick=" deletarEquipe(${equipe.id})">Deletar</button>
+        </div>
+    `
+    elementoLista.innerHTML = content;
+}
+
+let aux = null;
+
+function atualizarEquipes(id) {
+    const equipe = equipeService.listarEquipesPorId(id);
+
+    document.getElementById("nomeDaEquipe").value = equipe.nome;
+    document.getElementById("quantidade").value = equipe.quantidade;
+
+    document.getElementById("botaoCadastrar").classList.add("hidden");
+    document.getElementById("botaoEditar").classList.remove("hidden");
+}
+
+function editarEquipe() {
+    const nome = document.getElementById("nomeDaEquipe").value
+    const quantidade = document.getElementById("quantidade").value
+
+    equipeService.atualizarEquipes(aux, nome, quantidade);
+
+    listarEquipes();
+
+    document.getElementById("botaoCadastrar").classList.remove("hidden");
+    document.getElementById("botaoEditar").classList.add("hidden");
+
+    document.getElementById('listarEquipesUnica').classList.add('hidden');
+    aux = null;
+}
+
+function limparinputs() {
+    document.getElementById("nomeDaEquipe").value = '';
+    document.getElementById("quantidade").value = '';
+}
+
+function deletarEquipe(id){
+    equipeService.deletarEquipe(id);
+    listarEquipes();
+    document.getElementById('listarEquipesUnica').classList.add('hidden');
+}
+
